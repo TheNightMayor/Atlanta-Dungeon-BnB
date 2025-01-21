0be195b1515e5049ab8cf1f2073fefd8044ed131
Lawrence LeBlanc <lawrencekeeganleblanc@gmail.com>
2025-01-21 00:28:50 -0600

    sanity schema update

--- schemaTypes/booking.ts
+++ schemaTypes/booking.ts
@@ -65,7 +65,24 @@ const booking = {
             type: "number",
             validation: Rule =>Rule.required().min(0),
         }),
-    ]
+    ],
+    preview: {
+        select: {
+            name: 'user.name',
+            room: 'hotelRoom.name',
+            checkinDate: 'checkinDate',
+            checkoutDate: 'checkoutDate',
+            image: 'hotelRoom.coverImage.image'
+        },
+        prepare(selection) {
+            const {name, room, checkinDate, checkoutDate, image} = selection
+            return {
+                title: `${name} ${room}`,
+                subtitle: `${checkinDate} - ${checkoutDate}`,
+                media: image,
+        }
+    }
+    }
 }
 
 export default booking;
