992fc07c9c448e6889fe1481fd3d97cc86a6b31a
Lawrence LeBlanc <lawrencekeeganleblanc@gmail.com>
2026-04-23 19:28:26 -0500

    fix CTA text

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -53,7 +53,7 @@ const Home = async () => {
    {/* Call To Action */}
    <section className="container mx-auto py-8 px-4 md:px-0 border-2 border-tertiary-dark rounded-lg ">
      <div className="max-w-4xl mx-auto text-center">
-       <h3 className="text-2xl font-orbitron mb-4">Click here to begin your journey</h3>
+       <h3 className="text-2xl font-orbitron mb-4">Curious? Click here to begin your journey</h3>
        <Link href="/rooms" className="inline-block bg-primary text-white px-24 py-6 rounded-lg hover:bg-white dark:hover:bg-black text-xl font-bold transition border-2 border-tertiary-dark">Book Your Stay</Link>
      </div>
    </section>
